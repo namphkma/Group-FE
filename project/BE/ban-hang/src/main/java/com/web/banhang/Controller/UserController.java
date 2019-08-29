b31103795ec140a203ba49e98c13cccfcbe13d85
Nam PH <namph@ows.com.vn>
2019-08-29 23:19:53 +0700

    [BE-0] demo UserDao

--- project/BE/ban-hang/src/main/java/com/web/banhang/Controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.web.banhang.Controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-}
--- project/BE/ban-hang/src/main/java/com/web/banhang/Entity/User.java
+++ project/BE/ban-hang/src/main/java/com/web/banhang/Entity/User.java
@@ -1,4 +1,20 @@
 package com.web.banhang.Entity;
 
 public class User {
+
+    private String HoTen;
+
+    public User(String hoTen) {
+        HoTen = hoTen;
+    }
+
+
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        HoTen = hoTen;
+    }
 }
