30201e5440b5e725aeba74a46121a6b0ecdeaf12
Phạm Hảo Nam <31857149+namphkma@users.noreply.github.com>
2019-09-25 01:33:47 +0700

    Be web 1 (#10)
    * [BE-1] CRUD User
    
    * [BE-1] CRUD User
    
    * CRUD User
    
    * [BE-web-1] Fix folder
    
    * Delete .gitignore
    
    * add gitignore

--- project/BE/ban-hang/src/main/java/com/web/banhang/Service/Impl/UserService.java
+++ project/BE/ban-hang/src/main/java/com/web/banhang/Service/Impl/UserService.java
@@ -2,7 +2,9 @@ package com.web.banhang.Service.Impl;
 
 import com.web.banhang.Dao.IUserDao;
 import com.web.banhang.Entity.User;
+import com.web.banhang.Service.Dto.UserDto;
 import com.web.banhang.Service.IUserService;
+import com.web.banhang.Service.Mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +13,9 @@ import java.util.List;
 public class UserService implements IUserService {
 
     private IUserDao userDao;
-
-    public UserService(IUserDao userDao){
+    private UserMapper userMapper;
+    public UserService(IUserDao userDao, UserMapper userMapper){
+        this.userMapper = userMapper;
         this.userDao = userDao;
     }
 
@@ -22,7 +25,26 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public List<User> getList() {
-        return userDao.getList();
+    public List<UserDto> getList() {
+        return userMapper.mapToDto(userDao.getList());
+    }
+
+    @Override
+    public UserDto getUserById(int idUser){
+        return userMapper.mapToDto(userDao.getUserById(idUser));
+    }
+    @Override
+    public Integer deleteUser(int idUser){
+        return userDao.deleteUser(idUser);
+    }
+
+    @Override
+    public void insertUser(User user){
+       userDao.insertUser(user);
+    }
+
+    @Override
+    public Integer updateUser(User user){
+         return userDao.updateUser(user);
     }
 }
